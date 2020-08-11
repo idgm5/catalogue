321e3cd08d37d5bad5049091f2c8b5f3463d6ba6
idgm5 <idgm@pm.me>
2020-08-11 16:30:10 -0500

    delete unused actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,7 @@
 export const SEARCH_ITEM = 'SEARCH_ITEM';
 
-export const CATEGORIES = category => ({
-  type: 'CATEGORY', category
-});
-
-export const MEALS = meal => ({
-  type: 'MEAL', meal
-});
-
 export const SEARCH = item => ({
   type: SEARCH_ITEM, item
 });
 
-export default CATEGORIES;
+export default SEARCH;
--- src/reducers/meals.js
+++ src/reducers/meals.js
@@ -1,14 +1,7 @@
-import { CATEGORIES, MEALS, SEARCH_ITEM } from '../actions/index';
+import { SEARCH_ITEM } from '../actions/index';
 
 function meals(state = [], action) {
   switch (action.type) {
-    case CATEGORIES:
-      return [
-        ...state,
-        action.category,
-      ];
-    case MEALS:
-      return state;
     case SEARCH_ITEM:
         if(action.item === null){
           return "Try another ingredient or meal."
